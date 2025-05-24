19eedbd73d11cb383faabe3f291ee678ed13862f
Chandrika-Karri <chandrika.kovvuri@gmail.com>
2025-05-24 18:51:13 +0200

    service class

--- src/main/java/com/example/javawebdemo/controllers/HomeController.java
+++ src/main/java/com/example/javawebdemo/controllers/HomeController.java
@@ -1,32 +1,53 @@
 package com.example.javawebdemo.controllers;
 
-import com.example.javawebdemo.models.HockeyPlayerRepo;
+import com.example.javawebdemo.service.FizzBuzzService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController extends BaseController {
 
     @Autowired
-    private HockeyPlayerRepo hockeyPlayerRepo;
+    private FizzBuzzService fizzBuzzService;
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, Authentication authentication) {
+        boolean isAuthenticated = authentication != null && authentication.isAuthenticated() &&
+                !(authentication instanceof AnonymousAuthenticationToken);
+        model.addAttribute("isAuthenticated", isAuthenticated);
 
-        model.addAttribute("players", hockeyPlayerRepo.findAll());
-        model.addAttribute("user", "Chandrika");
+        if (isAuthenticated) {
+            DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+            model.addAttribute("email", user.getAttribute("email"));
+        }
 
         return "home";
     }
+    @PostMapping("/")
+    String homePost(@RequestParam int number, Model model,Authentication authentication) {
+        String fizzBuzz = fizzBuzzService.fizzBuzz(number);
+        model.addAttribute("number", number);
+        model.addAttribute("fizzBuzz", fizzBuzz);
 
-    @GetMapping("/profile")
-    public String profile(Model model) {
-        String user=getLoggedInEmail();
-        model.addAttribute("user", user);
-        return "profile";
+        // Set isAuthenticated and email just like in GET
+        boolean isAuthenticated = authentication != null && authentication.isAuthenticated() &&
+                !(authentication instanceof AnonymousAuthenticationToken);
+        model.addAttribute("isAuthenticated", isAuthenticated);
+
+        if (isAuthenticated) {
+            DefaultOAuth2User user = (DefaultOAuth2User) authentication.getPrincipal();
+            model.addAttribute("email", user.getAttribute("email"));
+        }
+
+        return "home";
+
+
     }
 }
+
